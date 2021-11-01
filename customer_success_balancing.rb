7c52468e29fd323f62718cc1f50fe2dbc8a18331
aureliomatsunaga <aureliomatsunaga@gmail.com>
2021-11-01 16:04:27 -0300

    Small refactor

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -10,7 +10,7 @@ class CustomerSuccessBalancing
 
   # Returns the ID of the customer success with most customers
   def execute
-    sorted_customer_success = customer_success_by_customers_count(customer_success_with_customers_count)
+    sorted_customer_success = customer_success_by_customers_count
     return 0 if sorted_customer_success[0][:customers_count] == sorted_customer_success[1][:customers_count]
 
     sorted_customer_success[0][:id]
@@ -22,21 +22,21 @@ class CustomerSuccessBalancing
     @customer_success.reject { |cs| @away_customer_success.include?(cs[:id]) }.sort { |a, b| b[:score] <=> a[:score] }
   end
 
-  def minimum_score_for_customer_success(customer_success, index)
-    return 0 if customer_success == @available_customer_success.last
-
-    @available_customer_success[index + 1][:score]
+  def customer_success_by_customers_count
+    customer_success_with_customers_count.sort { |a, b| b[:customers_count] <=> a[:customers_count] }
   end
 
   def customer_success_with_customers_count
     @available_customer_success.each_with_index.map do |acs, idx|
       minimum_score = minimum_score_for_customer_success(acs, idx)
       served_customers = @customers.select { |c| c[:score] <= acs[:score] && c[:score] >= minimum_score }
-      { id: acs[:id], customers_count: served_customers.length, score: acs[:score] }
+      { id: acs[:id], customers_count: served_customers.length }
     end
   end
 
-  def customer_success_by_customers_count(customer_success_with_customers_count)
-    customer_success_with_customers_count.sort { |a, b| b[:customers_count] <=> a[:customers_count] }
+  def minimum_score_for_customer_success(customer_success, index)
+    return 0 if customer_success == @available_customer_success.last
+
+    @available_customer_success[index + 1][:score]
   end
 end
